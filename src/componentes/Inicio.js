d29453ad83d22d3675b05745b8af40cdfe1b5f67
jhoncabrera2050 <jhoncabrera2050@gmail.com>
2023-08-21 20:39:02 -0500

    actualizand inicio

--- src/componentes/Inicio.js
+++ src/componentes/Inicio.js
@@ -1,14 +1,16 @@
 import React from 'react'
-
+import {Link} from 'react-router-dom'
 export const Inicio = () => {
   return (
     <div className='home'>
       <h1>
-        Soy un  <strong> backend/front end developer </strong>con más de 1 año de experiencia. Mi enfoque es desarrollar software altamente escalable con metodologías
-        ágiles. Tengo experiencia en tecnologías web como HTML, CSS y JavaScript, y uso
-        frameworks y <strong> herramientas de desarrollo  </strong>. También tengo habilidades en resolución de
-        problemas y gestión de proyectos, y trabajo bien en equipo.
+        Hola, Soy <strong> Joseph Cabrera  </strong>  soy desarrollador web y back end en Perú, y ofrezco mis
+        servicios de <strong> programación </strong> y <strong> desarrollo </strong>  en todo tipo de proyectos Tecnológicos.
+        ágiles.
       </h1>
+      <h2>Te ayudaré en proyectos de empresariales, requerimientos especificos de empresa o en crear tu sitio o aplicacion web.
+        <Link to="/contacto">Contacta Conmigo</Link>
+      </h2>
     </div>
   )
 }
